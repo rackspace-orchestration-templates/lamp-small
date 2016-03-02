2027ce2cbca09d4fb5488d5f194ce7357314368e
Chris Hultin <chris.hultin@rackspace.com>
2016-03-02 12:19:23 -0600

    Configuring artifacts to pull syslog/messages and cloud-init logs

--- test/fabric/lamp.py
+++ test/fabric/lamp.py
@@ -14,15 +14,19 @@ def check():
     assert process.is_up('php'), 'php is not running'
     assert service.is_enabled('httpd'), 'apache is not enabled'
     assert service.is_enabled('php'), 'php is not enabled'
-  elif (env.platform_family == 'debian'): 
-    assert process.is_up('apache2'), 'apache is not running' 
+  elif (env.platform_family == 'debian'):
+    assert process.is_up('apache2'), 'apache is not running'
     assert process.is_up('php5'), 'php is not running'
     assert service.is_enabled('apach2e'), 'apache is not enabled'
-    assert service.is_enabled('php5'), 'php is not enabled' 
+    assert service.is_enabled('php5'), 'php is not enabled'
   if ("secondary" not in socket.gethostname()):
     assert service.is_enabled('lsyncd'), 'lsyncd is not enabled'
 
 @task
 def artifacts():
   env.platform_family = detect.detect()
-  get_artifacts()
+  artifacts = ['/var/log/messages',
+              '/var/log/syslog',
+              '/var/log/cloud-init.log',
+              '/var/log/cloud-init-output.log']
+  get_artifacts(artifacts=artifacts)
